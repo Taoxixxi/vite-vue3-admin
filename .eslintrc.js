820e200fca159e5086f141ce1579ef7c16648184
taoming <taom@meixing.com>
2022-06-29 15:04:16 +0800

    feat: 测试cz

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
---
+++ src/router/index.js
@@ -0,0 +1,6 @@
+import {createRouter, createWebHashHistory} from 'vue-router'
+const routes = []
+export default createRouter({
+  history: createWebHashHistory(),
+  routes
+})
